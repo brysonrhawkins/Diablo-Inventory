8c4318fcd9474cca62e17364ee2ae1200a5c0624
Bryson Hawkins <64433149+brysonrhawkins@users.noreply.github.com>
2021-07-14 23:02:29 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import CharacterEquiped from './Components/Equiped/blankInventory';
+
+//Helmets
+const helmetOne = {name: 'Veil of Steel',armor: 21,MagicProps: null,rarity: 'Common'}
+const helmetTwo = {name: 'Leather Hood',armor: 22,MagicProps: null,rarity: 'Magical'}
+const helmetThree = {name: 'Prides Fall',armor: 38,MagicProps: null,rarity: 'Legendary'}
+const helmetFour = {name: 'Chain Hood',armor: 72,MagicProps: null,rarity: 'Rare'}
+
+//Shoulders
+const shouldersOne = {name: 'Pauldrons of the Skeleton King',armor: 58,MagicProps: null,rarity: 'Legendary'}
+const shouldersTwo = {name: 'Shoulder Gaurds',armor: 48,MagicProps: null,rarity: 'Common'}
+const shouldersThree = {name: 'Razeths Volition',armor: 68,MagicProps: null,rarity: 'Legendary'}
+const shouldersFour = {name: 'Borns Impunity',armor: 62,MagicProps: null,rarity: 'Set Item'}
+
+//Torso
+const torsoOne = {name: 'Cloth Tunic',armor: 3,MagicProps: null,rarity: 'Common'}
+const torsoTwo = {name: 'Leather Doublet',armor: 18,MagicProps: null,rarity: 'Common'}
+const torsoThree = {name: 'Hide Tunic',armor: 27,MagicProps: null,rarity: 'Magical'}
+const torsoFour = {name: 'Aquila Cuirass',armor: 68,MagicProps: null,rarity: 'Legendary'}
+
+//Cloaks
+const cloakOne = {name: 'Common Cape',armor: 60,MagicProps: null,rarity: 'Common'}
+const cloakTwo = {name: 'Tabard', armor: 84, MagicProps: null, rarity: 'Common'}
+const cloakThree = {name: 'Cloak of Deception',armor: 35,MagicProps: null,rarity: 'Legendary'}
+const cloakFour = {name: 'Beckon Sail',armor: 217,MagicProps: null,rarity: 'Legendary'}
+
+//Wrists
+const wristOne = {name: 'Bracers',armor: 9,MagicProps: null,rarity: 'Common'}
+const wristTwo = {name: 'Leather Cuffs',armor: 16,MagicProps: null,rarity: 'Common'}
+const wristThree = {name: 'Vambraces',armor: 33,MagicProps: null,rarity: 'Rare'}
+const wristFour = {name: 'Aughilds Ultimatum',armor: 102,MagicProps: null,rarity: 'Set Braces'}
+//Hands
+const handsOne = {name: 'Leather Gloves',armor: 16,MagicProps: null,rarity: 'Common'}
+const handsTwo = {name: 'Hide Gloves',armor: 32,MagicProps: null,rarity: 'Magic'}
+const handsThree = {name: 'Gloves of Worship',armor: 55,MagicProps: null,rarity: 'Legendary'}
+const handsFour = {name: 'Plated Gauntlets',armor: 87,MagicProps: null,rarity: 'Rare'}
+//Waist
+const waistOne = {name: 'Sash',armor: 6,MagicProps: null,rarity: 'Common'}
+const waistTwo = {name: 'Hide Belt',armor: 16,MagicProps: null,rarity: 'Magic'}
+const waistThree = {name: 'Saffron Wrap',armor: 127,MagicProps: null,rarity: 'Legendary'}
+const waistFour = {name: 'Hellcat WaistGaurd',armor: 265,MagicProps: null,rarity: 'Legendary'}
+//Legs
+const legsOne = {name: 'Cloth Pants',armor: 6,MagicProps: null,rarity: 'Common'}
+const legsTwo = {name: 'Rough Hide Leggings',armor: 27,MagicProps: null,rarity: 'Magic'}
+const legsThree = {name: 'Plate Leggings',armor: 101,MagicProps: null,rarity: 'Rare'}
+const legsFour = {name: 'Chausses',armor: 191,MagicProps: null,rarity: 'Rare'}
+//Feet
+const feetOne = {name: 'Shoes',armor: 7,MagicProps: null,rarity: 'Common'}
+const feetTwo = {name: 'Boots',armor: 18,MagicProps: null,rarity: 'Magic'}
+const feetThree = {name: 'Lut Socks',armor: 57,MagicProps: null,rarity: 'Legendary'}
+const feetFour = {name: 'Silk Shoes',armor: 106,MagicProps: null,rarity: 'Rare'}
+//Inventory Lists 
+
+function AvailableItems (){
+    return(
+        <h1>PlaceHolder</h1>
+    )
+}
+
+export default AvailableItems;
---
+++ index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './Components/Header/Header';
+import CharacterEquiped from './Components/Equiped/CharacterEquiped';
+import ArmorRating from './Components/Equiped/ArmorRating';
+import AvailableItems from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+    <ArmorRating />
+    <CharacterEquiped />
+    <AvailableItems />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
